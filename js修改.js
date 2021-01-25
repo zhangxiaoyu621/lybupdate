f24879e46a0f78294f6d4cbe73fb167a162de390
zhangxiaoyu621 <74093007+zhangxiaoyu621@users.noreply.github.com>
2021-01-25 14:16:35 +0800

    Update js修改.js

--- js修改.js
+++ js修改.js
@@ -1,5 +1,5 @@
 function sub() {
-  var BaseURL = 'http://127.0.0.1:5000 '
+  var BaseURL = 'http://127.0.0.1:5000'
    var postRequest = new XMLHttpRequest();
    postRequest.withCredentials= true;
    postRequest.open("POST", BaseURL + '/users/set_my_information');
