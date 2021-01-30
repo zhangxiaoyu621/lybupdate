bb3f82a86b5a1551def27a19a5ce99aff50bb6ef
zhangxiaoyu621 <74093007+zhangxiaoyu621@users.noreply.github.com>
2021-01-30 14:42:48 +0800

    Update js.js

--- js.js
+++ js.js
@@ -32,7 +32,7 @@ function regist() {
     }
 }
 function turnin() {
-  var BaseURL = 'http://127.0.0.1:5000 '
+  var BaseURL = 'http://127.0.0.1:5000'
    var postRequest = new XMLHttpRequest();
    postRequest.withCredentials= true; 
    postRequest.open("POST", BaseURL + '/session');
