6b4782342ee28147ea30bdbeffaf0c713896c626
zhangxiaoyu621 <74093007+zhangxiaoyu621@users.noreply.github.com>
2021-03-12 23:30:20 +0800

    Update js.js

--- js.js
+++ js.js
@@ -7,7 +7,7 @@ var TurnToRegister = () => {
 	document.getElementById('form_login_div').style.display = "none";
 }
 function regist() {
-  var BaseURL = 'http://127.0.0.1:5000'
+  var BaseURL = 'http：//8.129.238.142/zzj'
    var postRequest = new XMLHttpRequest();
    postRequest.withCredentials= true;
    postRequest.open("POST", BaseURL + '/users');
@@ -32,7 +32,7 @@ function regist() {
     }
 }
 function turnin() {
-  var BaseURL = 'http://127.0.0.1:5000'
+  var BaseURL = 'http：//8.129.238.142/zzj'
    var postRequest = new XMLHttpRequest();
    postRequest.withCredentials= true; 
    postRequest.open("POST", BaseURL + '/session');
